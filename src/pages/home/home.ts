90eb719deca372d1598ae7cde6ec98fc2316a993
Sérgio Moura <smouracalmon@gmail.com>
2018-08-10 16:26:00 -0300

    Taking picture of the form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	private cameraOptions:CameraOptions;
+	public imgData:string;
 
-  }
+	constructor(
+		private camera:Camera
+	) {
+		this.cameraOptions = {
+			quality: 100,
+			destinationType: this.camera.DestinationType.DATA_URL,
+			encodingType: this.camera.EncodingType.JPEG,
+			mediaType: this.camera.MediaType.PICTURE,
+			targetWidth:1536, // <== 2 * 768
+			correctOrientation:false,
+			allowEdit:true
+		}
+	}
 
+	onGetFormPictureClick() {
+		this.camera.getPicture(this.cameraOptions)
+		.then(
+			(imageData) => {
+				this.imgData = 'data:image/jpeg;base64,' + imageData;
+			},
+			(err) => {
+			}
+		);
+	}
+
+	/**
+	 * Gets the first dark pixel of the picture (top left)
+	 */
 }
